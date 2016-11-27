c678cf21dc7a961dc9c5007b7247b9d3f37a26e8
Eva Michálková <Eva Michlkov>
2016-11-27 01:54:55 +0100

    err5gg3

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ export default class App extends Component {
         </div>
 
         <div className='w-100'>
-           <div className='flex w-100' style={{
+           <div className='flex w-100 items-center' style={{
             height: '42px',
             backgroundColor: '#213043',
 
