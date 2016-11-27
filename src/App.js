490ecf5e9ac124951735cebfa07aaa1604d410f6
Eva Michálková <Eva Michlkov>
2016-11-27 01:27:23 +0100

    Created flex boxes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,58 @@ import './App.css';
 export default class App extends Component {
   render() {
     return (
-      <div className='vh-100 w-100'>
+      <div className='vh-100 w-100 flex'>
+        <div >
+          <div>
+          photo
+          </div>
+              
+          <div>
+          inbox
+          </div>
+        
+          <div>
+          alarm
+          </div>
+        
+          <div>
+          timer
+          </div>
+        
+          <div>
+          stamp
+          </div>
+        </div>
+
+        <div>
+           <div className='flex'>
+             <div>
+               equipe
+             </div>
+
+              <div className='flex'>
+                <div>
+                search
+                </div>
+
+                <div>
+                notifications
+                </div>
+              </div>
+           </div>
+
+           <div>
+          
+              <div>
+              </div>
+
+              <div>
+              </div>
+           </div>
+
+         
+        </div>
+      
       </div>
     );
   }
