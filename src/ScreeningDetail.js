2c5ee6e9b6bb74e2ae90f7573d6c711cbebee3df
Michael Vakoc <mivakoc@microsoft.com>
2016-11-27 05:44:47 +0100

    azure not working console: util.js:37 Uncaught TypeError: fs.readFileSync is not a function(…)

--- src/ScreeningDetail.js
+++ src/ScreeningDetail.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
-
+import azure  from 'azure';
 
 export default class ScreeningDetails extends Component {
+
     render() {
+
+        var sendNotification = function (){
+
+           
+            var notificationHubService = azure.createNotificationHubService('Equipe', 'Endpoint=sb://equipe.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=wawiOibI+6VvrN+tfGoaJzk1rw+6g6JKIIl5tdl2vcs=');
+        notificationHubService.apns.send(
+            null,
+            {"aps":
+                {
+                "alert": "Hello from Mobile Services!"
+                }
+            },
+            function (error)
+            {
+                if (!error) {
+                    console.warn("Notification successful");
+         }
+     }
+ );
+        }
+
         return (
         <div
           className='flex flex-column justify-top white'
           style={{width: 200, height: 75, background: '#203044'}}>
           <div >
           <img src={this.props.src} className="profilePic"/>
-            {this.props.name}
-            <img src="https://cdn0.iconfinder.com/data/icons/math-business-icon-set/93/1_1-128.png" className="inviteBtn"/>
+            <span className="personName">{this.props.name}</span>
+            <img src="https://cdn0.iconfinder.com/data/icons/math-business-icon-set/93/1_1-128.png" className="inviteBtn" onClick={sendNotification}/>
           </div>
         </div>
         )
